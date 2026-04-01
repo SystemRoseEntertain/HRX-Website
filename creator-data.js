54ba02874894c797ed3f4e78c27c9e7d97623fe3
SystemRoseEntertain <System@rose-entertain.de>
2026-04-01 14:18:32 +0200

    Add files via upload

---
+++ creator-data.js
@@ -0,0 +1,139 @@
+const HRX_CREATORS = [
+  {
+    name: "RoninMartoku",
+    since: "03.2026",
+    image: "roninmartoku.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/roninmartoku",
+    tiktok: ""
+  },
+  {
+    name: "HyptryX",
+    since: "03.2026",
+    image: "hyptryx.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/hyptryx",
+    tiktok: "https://www.tiktok.com/@hyptryx_?lang=de-DE"
+  },
+  {
+    name: "CryoShoX",
+    since: "03.2026",
+    image: "cryoshox.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/cryoshox",
+    tiktok: ""
+  },
+  {
+    name: "berniiiboy",
+    since: "03.2026",
+    image: "berniiiboy.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/berniiiboy",
+    tiktok: "https://www.tiktok.com/@berniiiboy"
+  },
+  {
+    name: "pedapeter",
+    since: "03.2026",
+    image: "pedapeter.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "",
+    tiktok: "https://www.tiktok.com/@pedapeter"
+  },
+  {
+    name: "tommecs",
+    since: "03.2026",
+    image: "tommecs.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/tommecs",
+    tiktok: "https://www.tiktok.com/@tommecs"
+  },
+  {
+    name: "WalkingDavidson",
+    since: "03.2026",
+    image: "walkingdavidson.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/walkingdavidson",
+    tiktok: ""
+  },
+  {
+    name: "LinkTheBeast",
+    since: "03.2026",
+    image: "linkthebeast.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "",
+    tiktok: "https://www.tiktok.com/@linkthebeast7"
+  },
+  {
+    name: "n_addl",
+    since: "03.2026",
+    image: "naddl_twitch.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/n_addl",
+    tiktok: ""
+  },
+  {
+    name: "NeoTano",
+    since: "03.2026",
+    image: "neotano.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/neotano",
+    tiktok: ""
+  },
+  {
+    name: "Hey Basti",
+    since: "03.2026",
+    image: "hey_basti.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/hey_basti_",
+    tiktok: "https://www.tiktok.com/@hey_basti_"
+  },
+  {
+    name: "JasiVali",
+    since: "03.2026",
+    image: "jasivali.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/jasivali",
+    tiktok: ""
+  },
+  {
+    name: "gta_glitch63",
+    since: "03.2026",
+    image: "gta_glitch63.png",
+    badge: "Creator",
+    desc: "Profil folgt in Kürze.",
+    twitch: "https://www.twitch.tv/gta_glitch63",
+    tiktok: ""
+  }
+];
+
+const HRX_LURK_STREAMERS = [
+  "hyptryx",
+  "austriadeluxeedition",
+  "derrchrisz",
+  "heybasti",
+  "matomatv",
+  "berniiiboy",
+  "walkingdavidson",
+  "neotano",
+  "angeluscorex",
+  "tommecs",
+  "letz_zock",
+  "roninmartoku",
+  "link_thebeast",
+  "jasivali",
+  "pedapeter",
+  "cryoshox",
+  "n_addl"
+];
